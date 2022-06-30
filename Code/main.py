eab94fbfdaec318f97b321a4458a573850443849
seidlech <seidlech@hs-albsig.de>
2022-06-30 23:01:44 +0200

    Erstellen des main.py-Files, Anlegen der Datensätze
    Signed-off-by: seidlech <seidlech@hs-albsig.de>

---
+++ Code/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Erstellt am 30.06.2022
+
+@author: Nils Heimbach, Christian T. Seidler
+
+Das Hauptskript. Soll später als Jupyter-Notebook umgesetzt werden.
+"""
+
+
+def main_nils():
+    pass
+
+
+def main_christian():
+    pass
+
+
+if __name__ == '__main__':
+    main_nils()
+    main_christian()
