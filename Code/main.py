f968f19654e0f1621fdd00a3653ad157b19708cd
nils12344 <heimbachnils@yahoo.de>
2022-07-01 13:29:40 +0200

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -6,9 +6,8 @@ Erstellt am 30.06.2022
 
 Das Hauptskript. Soll später als Jupyter-Notebook umgesetzt werden.
 """
-import pandas as pd
-import config as c
 
+import config as c
 import os
 import pandas as pd
 
@@ -42,26 +41,29 @@ def main_nils():
     df = load_temp(c.PATH+"TemperaturZeitreihe.json")
     print(df)
 
+    # joining dataframe temperatur and dataframe co2
+    # df_cd = pd.merge(df_temp, df_co2, how='inner', on = 'Date')
+
 
 def main_christian():
     FILENAME = 'fossil-fuel-co2-emissions-by-nation.csv'
     DIRECTORY = r'../Data/Input'
     filepath = os.path.join(DIRECTORY, FILENAME)
-    
+
     # Datensatz einlesen
     df_input = pd.read_csv(filepath)
-    
+
     # Entfernen aller überflüssigen Spalten
     df = df_input.iloc[:, :3]
-    
+
     # Extrahieren von Deutschland
-    df = df[(df['Country'] == 'GERMANY') | 
+    df = df[(df['Country'] == 'GERMANY') |
             (df['Country'] == 'FORMER GERMAN DEMOCRATIC REPUBLIC') |
             (df['Country'] == 'FEDERAL REPUBLIC OF GERMANY')]
-    
+
     # Jahr als Index setzen
     df = df.set_index('Year')
-        
+
     # Zusammenfügen der beiden Werte von 1945 bis 1990
     # Kopie erstellen und filtern
     df_sum_emissions = pd.DataFrame(index=df.index.copy())
@@ -76,19 +78,19 @@ def main_christian():
         # Jahre 1945-1990 in einen Dataframe packen
         df_sum_emissions = pd.concat([df_sum_emissions, df_year])
     df_sum_emissions.set_index('Year', inplace=True)
-    
+
     # Zusammenfügen der Daten
     df = df.drop(index=range(1945, 1991))
     df.reset_index(inplace=True)
-    
+
     df_sum_emissions = df_sum_emissions.reset_index()
     df_summed = pd.concat([df, df_sum_emissions])
-        
+
     df_summed = df_summed[df_summed['Country'] == 'GERMANY']
-    
+
     df_summed = df_summed.sort_values(by='Year').set_index('Year')
     print(df_summed.loc[1946])
-    
+
     df_final = df_summed.reset_index()
     print(df_final)
 
